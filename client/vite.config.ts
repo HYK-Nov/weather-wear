ac024c837f3c8994eee442dc2334472e9409d144
HYK <yundori97@gmail.com>
2025-04-09 00:50:36 +0900

    fix: 빌드 설정 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), tailwindcss()],
     build: {
-      outDir: "dist",
+      outDir: "client/dist",
     },
     resolve: {
       alias: {
