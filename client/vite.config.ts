85c3798fa3c38714a9ee877b4a04565edc18cec9
HYK <yundori97@gmail.com>
2025-04-09 02:42:23 +0900

    fix: 빌드 설정 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": path.resolve(__dirname, "./api"),
       },
     },
     assetsInclude: ["**/*.xlsx"], // .xlsx 파일을 Vite 에셋으로 포함
